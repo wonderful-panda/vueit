121d5b83f02373da72c761c2abda9b3e3bd080f1
Iwata Hidetaka <iwata0303@gmail.com>
2016-05-10 02:22:26 +0900

    implementing VueComponent decorator

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,69 @@
-export function add(a: number, b: number): number {
-    return a + b;
+import "reflect-metadata";
+import * as Vue from "vue";
+
+interface AnnotatedOptions {
+    props: { [key: string]: vuejs.PropOption };
 }
+
+interface VueComponent {
+    (option?: vuejs.ComponentOption): ClassDecorator;
+    prop: (option?: vuejs.PropOption) => PropertyDecorator;
+}
+const AnnotatedOptionsKey = Symbol("vue-component-decorator:options");
+
+const internalHooks = [
+    "data",
+    "el",
+    "init",
+    "created",
+    "ready",
+    "beforeCompile",
+    "compiled",
+    "beforeDestroy",
+    "destroyed",
+    "attached",
+    "detached",
+    "activate"
+];
+
+function makeComponent(target: Function, option: vuejs.ComponentOption): Function | void {
+    option.name = option.name || target.name;
+    const proto = target.prototype;
+    Object.getOwnPropertyNames(proto).filter(name => name !== "constructor").forEach(name => {
+        // define hooks
+        if (internalHooks.indexOf(name) > -1) {
+            option[name] = proto[name];
+        }
+    });
+    const ann = Reflect.getOwnMetadata(AnnotatedOptionsKey, proto) as AnnotatedOptions;
+    if (ann != null) {
+        // define props
+        option.props = option.props || ann.props;
+    }
+    // find super
+    const superProto = Object.getPrototypeOf(proto);
+    const Super = (superProto instanceof Vue
+        ? (superProto.constructor as vuejs.VueStatic)
+        : Vue
+    );
+    return Super.extend(option);
+}
+
+function defineProp(target: Object, propertyKey: string, option: vuejs.PropOption) {
+    let ann = Reflect.getOwnMetadata(AnnotatedOptionsKey, target) as AnnotatedOptions;
+    if (ann == null) {
+        ann = { props: {} };
+        Reflect.defineMetadata(AnnotatedOptionsKey, ann, target);
+    }
+    ann.props[propertyKey] = option;
+}
+
+const vueComponent: any = (option?: vuejs.ComponentOption): ClassDecorator => {
+    return target => makeComponent(target, option || {});
+};
+
+vueComponent.prop = function (option?: vuejs.PropOption): PropertyDecorator {
+    return (target, propertyKey) => defineProp(target, propertyKey.toString(), option || {});
+};
+
+export default vueComponent as VueComponent;
--- test/test.ts
+++ test/test.ts
@@ -1,11 +1,83 @@
 import * as assert from "power-assert";
-import { add } from "../lib/index";
+import * as Vue from "vue";
+import VueComponent from "../lib/index";
+import { jsdom } from "jsdom";
 
-describe("test", function() {
-    it("will succeed", function() {
-        assert(add(1, 1) === 2);
+global["document"] = jsdom("<html><body>dummy document</body></html>");
+global["window"] = document.defaultView;
+
+describe("vue-component-decorator", function () {
+    describe("hooks", function () {
+        @VueComponent()
+        class Base extends Vue {
+            created_: boolean;
+            destroyed_: boolean;
+            data(): any { return { created_: false, destroyed_: false }; }
+            created() { this.created_ = true; }
+            destroyed() { this.destroyed_ = true; }
+        };
+        it("basic", function () {
+            const c = new Base();
+            assert(c.created_ === true);
+            assert(c.destroyed_ === false);
+            c.$destroy();
+            assert(c.destroyed_ === true);
+        });
+
+        @VueComponent()
+        class Extended extends Base {
+            created_ex: boolean;
+            destroyed_ex: boolean;
+            data() { return { created_ex: false, destroyed_ex: false }; }
+            created() { this.created_ex = true; }
+            destroyed() { this.destroyed_ex = true; }
+        }
+        it("extended - hooks of both Base and Extended are called", function () {
+            const c = new Extended();
+            assert(c.created_ === true && c.destroyed_ === false);
+            assert(c.created_ex === true && c.destroyed_ex === false);
+            c.$destroy();
+            assert(c.destroyed_ === true);
+            assert(c.destroyed_ex === true);
+        });
     });
-    it("will fail", function() {
-        assert(add(1, 1) === 3);
+
+    describe("props", function () {
+        @VueComponent({ template: "<div>Test</div>" })
+        class Basic extends Vue {
+            @VueComponent.prop()
+            msg1: string;
+            @VueComponent.prop({ default: "value2default" })
+            msg2: string;
+        }
+        it("basic", function () {
+            const vm = new Vue({
+                template: "<div><test v-ref:target msg1='value1' /></div>",
+                components: {test: Basic}
+            });
+            vm.$mount();
+            const c = vm.$refs["target"] as Basic;
+            assert(c.msg1 === "value1");
+            assert(c.msg2 === "value2default");
+        });
+
+        @VueComponent()
+        class Extended extends Basic {
+            @VueComponent.prop({ default: "value2extended" })
+            msg2: string;
+            @VueComponent.prop()
+            msg3: string;
+        }
+        it("extended - props from both Basic and Extended are enabled", function () {
+            const vm = new Vue({
+                template: "<div><test v-ref:target msg1='value1' msg3='value3' /></div>",
+                components: {test: Extended}
+            });
+            vm.$mount();
+            const c = vm.$refs["target"] as Extended;
+            assert(c.msg1 === "value1");
+            assert(c.msg2 === "value2extended");
+            assert(c.msg3 === "value3");
+        });
     });
 });
